c3066773bfd64e2810094765d6ebfff645014f84
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2026-01-15 12:40:14 +0800

    add k8s command

---
+++ destroy-k8s.sh
@@ -0,0 +1,4 @@
+kubectl delete -f k8s/base/namespace.yml
+kubectl delete -f k8s/base/config/configmap.yml
+kubectl delete -f k8s/base/config/secrets.yml
+kubectl delete -f k8s/base/deployments/services/
